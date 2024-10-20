df83563afdbd9105dafd027fdcc9c57cac81a8a1
Pradeep V <pradeep.venkateshwaran@rasiinfotech.com>
2024-10-20 19:24:23 +0530

    cosm correction

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -17,7 +17,7 @@ const NavBar = ({setShowLogin}) => {
             <ul className="navbar-menu">
                 <Link to='/' onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>home</Link>
                 <a href='#explore-menu' onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : "" }>menu</a>
-                <a href='#app-download' onClick={() => setMenu("mobile-app")} className={menu === "mobile-app" ? "active" : "" }>mobile-app</a>
+                <a href='#app-download' onClick={() => setMenu("mobile-app")} className={menu === "mobile-app" ? "active" : "" }>Rider Food App</a>
                 <a href='#footer' onClick={() => setMenu("contact us")} className={menu === "contact us" ? "active" : "" }>contact us</a>
             </ul>
 
